aa63792437a2b6bce2ce99805bf0dee7394e6200
Satish Venkatakrishna <satish860@gmail.com>
2018-07-06 11:56:15 +0530

    Added a custom webapplication factory . May be or may not be neede

---
+++ PrivyGet.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace PrivyGet.Tests
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> 
+        where TStartup : class
+    {
+        public CustomWebApplicationFactory()
+        {
+            
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("development");
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
